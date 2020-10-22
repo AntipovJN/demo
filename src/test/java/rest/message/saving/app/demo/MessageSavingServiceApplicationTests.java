86dfee523517f3918cdea2f8fa713d84387f983f
Eugene Antipov <eugene.antipenko@scalors.com>
2020-10-23 02:09:04 +0300

    Re-implement messageSendingTest()

--- src/test/java/rest/message/saving/app/demo/MessageSavingServiceApplicationTests.java
+++ src/test/java/rest/message/saving/app/demo/MessageSavingServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package rest.message.saving.app.demo;
 
+import javafx.util.Pair;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -24,7 +25,6 @@ class MessageSavingServiceApplicationTests {
     private static final String GET_MESSAGES_URI = "http://localhost:8080/messages?page=%s";
     private static final String GET_NOTIFICATIONS_URI = "http://localhost:8080/notifications?page=%s";
     private static final int MESSAGES_COUNT = 1000;
-
     private HttpHeaders headers;
     private RestTemplate restTemplate;
 
@@ -36,45 +36,58 @@ class MessageSavingServiceApplicationTests {
     }
 
     @Test
-    void messageSendingTest() {
+    void messageSendingTest() throws InterruptedException {
         List<Message> sentMessages = sentMessageSaveRequestsRequest();
 
         int deliveredCount = 0;
         int notDeliveredCount = 0;
-        int pagesCount = MESSAGES_COUNT / 10;
-        pagesCount = MESSAGES_COUNT % 10 == 0 ? --pagesCount : pagesCount;
+
+        waitUntilMessagesSave();
+        Pair<List<Message>, List<Notification>> messagesAndNotifications = extractMessagesAndNotificationsFromApi(MESSAGES_COUNT / 10);
+        List<Message> messagesFromApi = messagesAndNotifications.getKey();
+        List<Notification> notificationsFromApi = messagesAndNotifications.getValue();
+
+        int notificationLack = messagesFromApi.size() - notificationsFromApi.size();
+        if (notificationLack > 0)
+            throw new RuntimeException(notificationLack + " notification was not created for Message with content:'%s'");
+        if (notificationLack < 0)
+            throw new RuntimeException(notificationLack + " notification was created for unsaved Message");
 
         for (Message sentMessage : sentMessages) {
-            boolean messageWasFind = false;
-            boolean notificationWasFind = false;
-            Notification foundNotification = null;
-
-            for (int page = pagesCount; page >= 0; page--) {
-                Optional<Message> optionalMessageFromApi = findMessage(sentMessage, page);
-                Optional<Notification> optionalNotificationFromApi = findNotification(page, sentMessage);
-                if (optionalMessageFromApi.isPresent()) messageWasFind = true;
-                if (optionalNotificationFromApi.isPresent()) {
-                    notificationWasFind = true;
-                    foundNotification = optionalNotificationFromApi.get();
-                }
-                if (messageWasFind && notificationWasFind) {
-                    if (foundNotification.getDelivered())
-                        deliveredCount++;
-                    else
-                        notDeliveredCount++;
-                    break;
-                }
-            }
-            if (messageWasFind && !notificationWasFind)
-                throw new RuntimeException(String.format("Notification was not created for Message with content:'%s'", sentMessage.getMessageContent()));
-            else if (!messageWasFind && notificationWasFind)
-                throw new RuntimeException(String.format("Notification was created for unsaved Message with content:'%s'", sentMessage.getMessageContent()));
-            else if (!messageWasFind)
-                notDeliveredCount++;
-
+            if (isMessagePresent(messagesFromApi, sentMessage) && isNotificationPresentAndDelivered(notificationsFromApi, sentMessage))
+                deliveredCount++;
+            else notDeliveredCount++;
         }
 
         logStatistic(deliveredCount, notDeliveredCount);
+    }
+
+    private boolean isMessagePresent(List<Message> messagesFromApi, Message sentMessage) {
+        return messagesFromApi.stream().anyMatch(m -> m.getMessageContent().equals(sentMessage.getMessageContent()));
+    }
+
+    private boolean isNotificationPresentAndDelivered(List<Notification> notificationsFromApi, Message sentMessage) {
+        return notificationsFromApi.stream().anyMatch(n -> n.getMessage().getMessageContent().equals(sentMessage.getMessageContent()) && n.getDelivered());
+    }
+
+    private void waitUntilMessagesSave() throws InterruptedException {
+        boolean isMessageSaved = false;
+        while (!isMessageSaved) {
+            isMessageSaved = Objects.requireNonNull(restTemplate.getForObject("http://localhost:8080/isready", Boolean.class));
+
+        }
+    }
+
+    private Pair<List<Message>, List<Notification>> extractMessagesAndNotificationsFromApi(int pagesCount) {
+        List<Message> messagesFromApi = new ArrayList<>();
+        List<Notification> notificationsFromApi = new ArrayList<>();
+
+        for (int page = 0; page < pagesCount; page++) {
+            messagesFromApi.addAll(Arrays.asList(getMessages(page)));
+            notificationsFromApi.addAll(Arrays.asList(getNotifications(page)));
+        }
+
+        return new Pair<>(messagesFromApi, notificationsFromApi);
     }
 
     private void logStatistic(int deliveredCount, int notDeliveredCount) {
@@ -100,16 +113,6 @@ class MessageSavingServiceApplicationTests {
             sentMessages.add(message);
         }
         return sentMessages;
-    }
-
-    private Optional<Notification> findNotification(int page, Message message) {
-        return Arrays.stream(getNotifications(page)).filter(n -> n.getMessage().getMessageContent().equals(message.getMessageContent())).findFirst();
-    }
-
-    private Optional<Message> findMessage(Message sentMessage, int page) {
-        return Arrays.stream(getMessages(page))
-                .filter(m -> m.getMessageContent().equals(sentMessage.getMessageContent()))
-                .findFirst();
     }
 
     private void saveMessage(Message message) {
